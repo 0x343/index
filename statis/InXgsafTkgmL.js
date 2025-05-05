6e068ae2ad237989dbcb6edab642c8a15029e4ba
0x343 <0x8567@gmail.com>
2025-05-05 09:04:30 +0700

    Create InXgsafTkgmL.js

---
+++ statis/InXgsafTkgmL.js
@@ -0,0 +1,274 @@
+document.getElementById("InXgsafTkgmL").innerHTML = `
+
+<!-- Page Title -->
+    <div class="page-title" data-aos="fade">
+      <div class="heading">
+        <div class="container">
+          <div class="row d-flex justify-content-center text-center">
+            <div class="col-lg-8">
+              <h2>What's Unique About Lotto Chain?</h2>
+              <p class="mb-0">Lotto Chain is a blockchain-based lotto & lottery platform that offers a transparent, secure and trusted gaming experience. Using decentralized technology, Lotto Chain ensures that every transaction, bet placed and payment is permanently recorded on the blockchain and cannot be manipulated.All lottery results are taken directly from official sources that are internationally recognized, which are already trusted.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End Page Title -->
+
+  <!-- Page Title -->
+    <div class="page-title" data-aos="fade">
+      <div class="heading">
+        <div class="container">
+          <div class="row d-flex justify-content-center text-center">
+            <div class="col-lg-8">
+              <h2>How Does Lotto Chain Work?</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End Page Title -->
+
+    <!-- Services Section -->
+    <section id="services" class="services section">
+
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-item  position-relative">
+              <div class="icon">
+                <i class="bi bi-cash-stack"></i>
+              </div>
+                <h3>Select Market & Bet</h3>
+              <p>Players choose the desired market (Hong Kong, Singapore, Sydney, China, Cambodia, or Bull Eye) and place a bet.</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="bi bi-broadcast"></i>
+              </div>
+                <h3>Official Results</h3>
+              <p>The draw results are taken directly from the official sources of each market, ensuring full transparency.</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="bi bi-shield-lock"></i>
+              </div>
+                <h3>Blockchain Security</h3>
+              <p>All transactions are recorded in the blockchain, decentralized, ensuring no manipulation or alteration of data.</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="bi bi-incognito"></i>
+              </div>
+                <h3>Anonymity & Privacy</h3>
+              <p>Players can participate without having to reveal personal identity, and without KYC, player privacy remains intact.</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="bi bi-lightning-charge"></i>
+              </div>
+                <h3>Fast & Efficient</h3>
+              <p>Without the involvement of a third party, transactions are fast and costs are lower than conventional lotteries.</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="bi bi-wallet2"></i>
+              </div>
+                <h3>No Initial Deposit</h3>
+              <p>Lotto Chain allows you to place bets without any initial deposit. Simply use the MetaMask wallet app, and you can start betting right away.</p>
+            </div>
+          </div><!-- End Service Item -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Services Section -->
+
+  <!-- About Section -->
+    <section id="about" class="about section">
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-4">
+          </div>
+          <div class="col-lg-8 content">
+            <h2>Benefits of Using Lotto Chain</h2><br>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-check-circle"></i> <span>Using Official Results</span></li>
+                  <li><i class="bi bi-check-circle"></i> <span>Transparent & Decentralized</span></li>
+                  <li><i class="bi bi-check-circle"></i> <span>Playing with Crypto</span></li>
+                  <li><i class="bi bi-check-circle"></i> <span>No Registration</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-check-circle"></i> <span>Smart Contract Security</span></li>
+                  <li><i class="bi bi-check-circle"></i> <span>Flexible Betting</span></li>
+                  <li><i class="bi bi-check-circle"></i> <span>Multi-Blockchain Support</span></li>
+                  <li><i class="bi bi-check-circle"></i> <span>Instant Payments</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </section><!-- /About Section -->
+
+  <!-- Page Title -->
+    <div class="page-title" data-aos="fade">
+      <div class="heading">
+        <div class="container">
+          <div class="row d-flex justify-content-center text-center">
+            <div class="col-lg-8">
+              <h2>Roadmap</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End Page Title -->
+
+    <!-- Resume Section -->
+    <section id="resume" class="resume section">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="resume-item">
+              <h4>I</h4>
+              <p>Functional development and testing.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>II</h4>
+              <p>The launch of the public communication Beta (Testnet) has been planned. The Beta stage of communication will be present on the testnet network.</p>
+            </div><!-- Edn Resume Item -->
+            
+            <div class="resume-item">
+              <h4>III</h4>
+              <p>Mainnet launch and international market integration.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>IV</h4>
+              <p>Introduction to the project and various services.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>V</h4>
+              <p>Notifications. The system will send automatic notifications to users who win bets. These notifications can be in the form of sound or visual elements that appear on the interface in real-time.</p>
+            </div><!-- Edn Resume Item -->
+            
+            <div class="resume-item">
+              <h4>VI</h4>
+              <p>Smart contract optimization and network scalability enhancement.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>VII</h4>
+              <p>Social Decentralizer Feature (Like). Users can give likes to certain betting activities or entries. This feature uses a decentralized approach, allowing social interaction without centralized control.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>VIII</h4>
+              <p>To create a decentralized platform for secure and transparent lottery and betting games on blockchain.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>IX</h4>
+              <p>Social Decentralizer Feature (Comments). Users can leave comments on certain betting entries or results, creating an open and transparent community discussion space.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>X</h4>
+              <p>Statistics & Data. Provides a statistics panel that displays important data such as bet amounts, total payouts, active players, recent winners, and average bet analysis. These statistics will be rendered visually and updated in real-time.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>XI</h4>
+              <p>To develop a blockchain-based ecosystem that ensures fairness and transparency in betting and lottery games.</p>
+            </div><!-- Edn Resume Item -->
+            
+            <div class="resume-item">
+              <h4>XII</h4>
+              <p>To implement a decentralized lottery and betting system that provides provably fair and verifiable outcomes.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>XIII</h4>
+              <p>To build a fully decentralized gambling experience on blockchain that guarantees security, transparency, and fairness.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>XIV</h4>
+              <p>To enhance the integrity of online betting and lottery through blockchain technology.</p>
+            </div><!-- Edn Resume Item -->
+            
+            <div class="resume-item">
+              <h4>XV</h4>
+              <p>Addition of multi-chain features and ecosystem expansion.</p>
+            </div><!-- Edn Resume Item -->
+            
+            <div class="resume-item">
+              <h4>XVI</h4>
+              <p>Development of innovative features and global partnerships to enhance the ecosystem.</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>XVII</h4>
+              <p>Coming Soon...</p>
+            </div><!-- Edn Resume Item -->
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- /Resume Section -->
+
+  <footer id="footer" class="footer dark-background">
+    <div class="container">
+      <h2 class="sitename">Join Now!</h2>
+      <br><button onclick="window.open('cs', '_blank')" style="background: linear-gradient(90deg, #03a9f4, #f441a5); -webkit-background-clip: text; -webkit-text-fill-color: transparent; border: none;"><i class="bi bi-play"></i>Play!</button><br><br>
+      <p>Be part of the blockchain lottery revolution. Experience the exciting experience of privacy, transparency, security, and speed in every bet. Try your luck now!</p>
+      <div class="social-links d-flex justify-content-center">
+        <a href="https://x.com/LottoChainXyz" target="_blank" rel="noopener noreferrer"><i class="bi bi-twitter-x"></i></a>
+        <a href="https://t.me/LottoChainXyZ" target="_blank" rel="noopener noreferrer"><i class="bi bi-telegram"></i></a>
+        <a href="https://facebook.com/LottoChainXyz" target="_blank" rel="noopener noreferrer"><i class="bi bi-facebook"></i></a>
+        <a href="mailto:support@lottochain.xyz" target="_blank" rel="noopener noreferrer"><i class="bi bi-envelope"></i></a>
+      </div>
+      <div class="container">
+        <div class="copyright">
+          <span>©</span> <strong class="px-1 sitename">Powered By Lotto Chain</strong> <span><a href="TermsAndConditions" style="color: #007bff; text-decoration: none;" target="_blank">2025 All Rights Reserved.</a> Play responsibly!</span>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+`;
